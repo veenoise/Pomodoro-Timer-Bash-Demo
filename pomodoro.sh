89e3afc2578e1dcab573ecf85d461ec73ce38385
veenoise <william.eduard123@gmail.com>
2024-05-09 11:50:39 +0800

    Added the scripts and music

---
+++ pomodoro.sh
@@ -0,0 +1,30 @@
+echo "Starting Pomodoro Timer"
+
+while [ 1 -eq 1 ]
+do
+	echo "Begin work"
+	for i in {0..24}
+	do
+		echo "min:" $i
+		sleep 60
+		echo -en "\033[1A\033[K"
+	done
+	
+	play ./notification/waterphone.mp3
+
+	for i in {1..13}; do echo -en "\033[1A\033[K"; done	
+
+	echo "Break time"
+
+	for i in {0..4}
+	do
+		echo "min:" $i
+		sleep 60
+		echo -en "\033[1A\033[K"
+	done
+
+	play ./notification/guitar-riff.mp3
+
+	for i in {1..13}; do echo -en "\033[1A\033[K"; done
+
+done
